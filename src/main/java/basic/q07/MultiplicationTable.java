6aa67e4da319de6ef946ef6698e33a33fab4f65e
GJPC\user <s-murakami@seassist.ne.jp>
2024-11-21 20:44:36 +0900

    first commit

---
+++ src/main/java/basic/q07/MultiplicationTable.java
@@ -0,0 +1,17 @@
+package basic.q07;
+
+public class MultiplicationTable {
+	public static void main(String[] arg) {
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 1; j <= 9; j++) {
+				int m = i * j;
+				System.out.print(" ");
+				if (10 > m) {
+					System.out.print(" ");
+				}
+				System.out.print(m);
+			}
+			System.out.println();
+		}
+	}
+}
